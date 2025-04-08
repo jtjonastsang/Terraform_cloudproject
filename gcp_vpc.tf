1f411b92c5c9b9ea2abcb6f20d686888b554a711
jtjonastsang <230329096@stu.vtc.edu.hk>
2025-04-08 16:56:42 +0800

    Add Kubernetes cluster to GCP

--- gcp_vpc.tf
+++ gcp_vpc.tf
@@ -23,3 +23,33 @@ resource "google_compute_subnetwork" "private2" {
   network       = google_compute_network.example.id
   private_ip_google_access = true
 }
+
+resource "google_container_cluster" "example" {
+  name     = "my-cluster"
+  location = "asia-east1"  # 同你嘅子網region一致
+  network    = google_compute_network.example.name
+  subnetwork = google_compute_subnetwork.private1.name
+
+  # 移除預設節點池，因為GKE要求自訂節點池
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  # 私有集群設定（用私有子網）
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false  # 設false可以用公網訪問
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.example.name
+  location   = "asia-east1"
+  node_count = 2  # 2個VM，滿足項目要求
+
+  node_config {
+    machine_type = "e2-medium"  # 小型機器，慳成本
+    disk_size_gb = 20
+  }
+}
